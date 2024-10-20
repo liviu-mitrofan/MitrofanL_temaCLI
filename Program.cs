d3f9c0c7adfbbfa782355a3810837ea061ca8a36
Liviu Mitrofan <liviujmk@gmail.com>
2024-10-20 15:01:04 +0300

    Added tema

--- Program.cs
+++ Program.cs
@@ -1,16 +1,86 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
 
-namespace MitrofanL_temaCLI
+namespace OpenTK_console_sample01
 {
-    internal class Program
+    class SimpleWindow : GameWindow
     {
+        private Vector2 trianglePosition = Vector2.Zero;
+        private float triangleSize = 0.5f;
+
+        public SimpleWindow() : base(800, 600)
+        {
+            KeyDown += Keyboard_KeyDown;
+        }
+
+        void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+                this.Exit();
+
+            if (e.Key == Key.F11)
+                if (this.WindowState == WindowState.Fullscreen)
+                    this.WindowState = WindowState.Normal;
+                else
+                    this.WindowState = WindowState.Fullscreen;
+
+            if (e.Key == Key.Up)
+                trianglePosition.Y += 0.1f;
+            if (e.Key == Key.Down)
+                trianglePosition.Y -= 0.1f;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(Color.MidnightBlue);
+            CursorVisible = true;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            var mouse = Mouse.GetState();
+
+            // Update triangle position based on mouse position
+            trianglePosition.X = (mouse.X / (float)Width) * 2 - 1;
+            trianglePosition.Y = -((mouse.Y / (float)Height) * 2 - 1);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            GL.Begin(PrimitiveType.Triangles);
+
+            GL.Color3(Color.MidnightBlue);
+            GL.Vertex2(trianglePosition.X - triangleSize / 2, trianglePosition.Y + triangleSize / 2);
+            GL.Color3(Color.SpringGreen);
+            GL.Vertex2(trianglePosition.X, trianglePosition.Y - triangleSize / 2);
+            GL.Color3(Color.Ivory);
+            GL.Vertex2(trianglePosition.X + triangleSize / 2, trianglePosition.Y + triangleSize / 2);
+
+            GL.End();
+
+            this.SwapBuffers();
+        }
+
+        [STAThread]
         static void Main(string[] args)
         {
-
+            using (SimpleWindow example = new SimpleWindow())
+            {
+                example.Run(30.0, 0.0);
+            }
         }
     }
 }
---
+++ tema.md
@@ -0,0 +1,10 @@
+﻿## Răspunsuri întrebări:
+
+1. Un viewport este o regiune dreptunghiulară a ecranului în care se va desena scena.
+2. Frames per second reprezintă numărul de cadre desenate pe secundă.
+3. Metoda OnUpdateFrame() este rulată înainte de metoda OnRenderFrame().
+4. Modul imediat de randare este un mod de randare în care se desenează direct pe ecran, fără a folosi un buffer intermediar.
+5. Ultima versiune de OpenGL care acceptă modul imediat este OpenGL 3.0.
+6. Metoda OnRenderFrame() este rulată după metoda OnUpdateFrame().
+7. Metoda OnResize() trebuie executată cel puțin o dată pentru a seta corect viewport-ul.
+8. Parametrii metodei CreatePerspectiveFieldOfView() sunt unghiul de vedere, raportul de aspect, distanța la care se vede cea mai apropiată parte a scenei și distanța la care se vede cea mai îndepărtată parte a scenei. Domeniul de valori pentru aceștia este: unghiul de vedere trebuie să fie în radiani, raportul de aspect trebuie să fie un număr real pozitiv, iar distanțele trebuie să fie numere reale pozitive.
