2c4a608478cf1ddd145cc8725640faf88dfafc39
Liviu Mitrofan <liviujmk@gmail.com>
2024-10-20 14:40:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MitrofanL_temaCLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
