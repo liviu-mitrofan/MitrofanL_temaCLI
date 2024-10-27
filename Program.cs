ea981c40feeaeba932a73607c84a5297be864c56
Liviu Mitrofan <liviu.mitrofan@student.usv.ro>
2024-10-27 20:51:03 +0200

    MitrofanL_temaLab3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.IO;
 using OpenTK;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
@@ -8,35 +9,104 @@ namespace OpenTK_console_sample01
 {
     class SimpleWindow : GameWindow
     {
-        private Vector2 trianglePosition = Vector2.Zero;
-        private float triangleSize = 0.5f;
+        // Coordonatele triunghiului
+        private readonly float[] x = new float[3];
+        private readonly float[] y = new float[3];
+
+        // Culorile pentru fiecare varf
+        private readonly float[] red = new float[3];
+        private readonly float[] green = new float[3];
+        private readonly float[] blue = new float[3];
+
+        // Pentru rotirea cu mouse-ul
+        private float unghi = 0.0f;
+        private bool mouseApasat = false;
+        private int mouseXVechi;
 
         public SimpleWindow() : base(800, 600)
         {
             KeyDown += Keyboard_KeyDown;
+            CitesteDateDinFisier("coordonate.txt");
+            GenereazaCuloriRandom();
+        }
+
+        private void CitesteDateDinFisier(string numeFisier)
+        {
+            try
+            {
+                string caleFisier = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, numeFisier);
+
+                string[] linii = File.ReadAllLines(caleFisier);
+
+                for (int i = 0; i < 3; i++)
+                {
+                    string[] coordonate = linii[i].Split(',');
+                    x[i] = float.Parse(coordonate[0]);
+                    y[i] = float.Parse(coordonate[1]);
+                }
+                Console.WriteLine("Coordonate citite cu succes!");
+            }
+            catch
+            {
+                Console.WriteLine("Nu s-a putut citi fisierul, se folosesc coordonate implicite!");
+                // Coordonate implicite pentru triunghi
+                x[0] = -0.5f; y[0] = 0.288f;
+                x[1] = 0.0f; y[1] = -0.577f;
+                x[2] = 0.5f; y[2] = 0.288f;
+            }
+        }
+
+        private void GenereazaCuloriRandom()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                red[i] = (float)rnd.NextDouble();
+                green[i] = (float)rnd.NextDouble();
+                blue[i] = (float)rnd.NextDouble();
+            }
+            AfiseazaCulori();
+        }
+
+        private void AfiseazaCulori()
+        {
+            Console.WriteLine("\nCulorile varfurilor:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Varf {i + 1}: R={red[i]:F2}, G={green[i]:F2}, B={blue[i]:F2}");
+            }
         }
 
         void Keyboard_KeyDown(object sender, KeyboardKeyEventArgs e)
         {
             if (e.Key == Key.Escape)
-                this.Exit();
+                Exit();
 
-            if (e.Key == Key.F11)
-                if (this.WindowState == WindowState.Fullscreen)
-                    this.WindowState = WindowState.Normal;
-                else
-                    this.WindowState = WindowState.Fullscreen;
+            Random rnd = new Random();
+            if (e.Key == Key.R)
+            {
+                for (int i = 0; i < 3; i++)
+                    red[i] = (float)rnd.NextDouble();
+            }
+            else if (e.Key == Key.G)
+            {
+                for (int i = 0; i < 3; i++)
+                    green[i] = (float)rnd.NextDouble();
+            }
+            else if (e.Key == Key.B)
+            {
+                for (int i = 0; i < 3; i++)
+                    blue[i] = (float)rnd.NextDouble();
+            }
 
-            if (e.Key == Key.Up)
-                trianglePosition.Y += 0.1f;
-            if (e.Key == Key.Down)
-                trianglePosition.Y -= 0.1f;
+            AfiseazaCulori();
         }
 
         protected override void OnLoad(EventArgs e)
         {
             GL.ClearColor(Color.MidnightBlue);
-            CursorVisible = true;
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
         }
 
         protected override void OnResize(EventArgs e)
@@ -44,42 +114,68 @@ namespace OpenTK_console_sample01
             GL.Viewport(0, 0, Width, Height);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
+            GL.Ortho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
             var mouse = Mouse.GetState();
 
-            // Update triangle position based on mouse position
-            trianglePosition.X = (mouse.X / (float)Width) * 2 - 1;
-            trianglePosition.Y = -((mouse.Y / (float)Height) * 2 - 1);
+            if (mouse.LeftButton == ButtonState.Pressed)
+            {
+                if (!mouseApasat)
+                {
+                    mouseApasat = true;
+                    mouseXVechi = mouse.X;
+                }
+                else
+                {
+                    unghi += (mouse.X - mouseXVechi) * 0.01f;
+                    mouseXVechi = mouse.X;
+                }
+            }
+            else
+            {
+                mouseApasat = false;
+            }
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Rotate(unghi * 180 / Math.PI, 0, 1, 0);
+
+            // Desenare triunghi normal
             GL.Begin(PrimitiveType.Triangles);
-
-            GL.Color3(Color.MidnightBlue);
-            GL.Vertex2(trianglePosition.X - triangleSize / 2, trianglePosition.Y + triangleSize / 2);
-            GL.Color3(Color.SpringGreen);
-            GL.Vertex2(trianglePosition.X, trianglePosition.Y - triangleSize / 2);
-            GL.Color3(Color.Ivory);
-            GL.Vertex2(trianglePosition.X + triangleSize / 2, trianglePosition.Y + triangleSize / 2);
-
+            for (int i = 0; i < 3; i++)
+            {
+                GL.Color3(red[i], green[i], blue[i]);
+                GL.Vertex2(x[i], y[i]);
+            }
             GL.End();
 
-            this.SwapBuffers();
+            // Desenare triunghi in mod strip (translatat)
+            GL.Translate(0.5f, 0.5f, 0);
+            GL.Begin(PrimitiveType.TriangleStrip);
+            for (int i = 0; i < 3; i++)
+            {
+                GL.Color3(red[i], green[i], blue[i]);
+                GL.Vertex2(x[i] * 0.5f, y[i] * 0.5f);
+            }
+            GL.End();
+
+            SwapBuffers();
         }
 
         [STAThread]
         static void Main(string[] args)
         {
-            using (SimpleWindow example = new SimpleWindow())
+            using (SimpleWindow window = new SimpleWindow())
             {
-                example.Run(30.0, 0.0);
+                window.Run(30.0);
             }
         }
     }
---
+++ raspunsuriLab3.md
@@ -0,0 +1,16 @@
+﻿Răspunsuri ex 4:
+- LineLoop desenează un poligon închis, conectând ultimul punct cu primul.
+- LineStrip desenează un poligon deschis, conectând fiecare punct cu următorul.
+- TriangleFan desenează un poligon închis, conectând primul punct cu fiecare pereche de puncte consecutive.
+- TriangleStrip desenează un poligon deschis, conectând fiecare pereche de puncte consecutive.
+
+Răspunsuri ex 7:
+- Un gradient de culoare reprezintă o tranziție de culoare între două sau mai multe culori.
+- Pentru a obține un gradient de culoare în OpenGL, se folosește funcția glShadeModel(GL_SMOOTH) pentru a activa interpolarea culorilor între vârfurile primitivei desenate.
+- Se specifică culorile vârfurilor primitivei folosind funcția glColor3f sau glColor4f.
+- OpenGL va interpola culorile între vârfuri, rezultând un gradient de culoare.
+
+
+Răspuns ex 10:
+
+Dacă se folosesc culori diferite pentru fiecare vârf al unei primitive desenate în modul strip, OpenGL va interpola culorile între vârfuri, rezultând un gradient de culoare pe întreaga lungime a primitivei.
